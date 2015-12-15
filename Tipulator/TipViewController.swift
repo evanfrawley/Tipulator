464fed74dce6b38039c3e065f251472d9ce48ef5
Evan J. Frawley <frawley@D-172-28-35-159.dhcp4.washington.edu>
2015-12-14 23:58:09 -0800

    made a viewcontroller correction

---
+++ Tipulator/TipViewController.swift
@@ -0,0 +1,74 @@
+//
+//  TipViewController.swift
+//  Tipulator
+//
+//  Created by Evan J. Frawley on 12/14/15.
+//  Copyright (c) 2015 Evan J. Frawley. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+    
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let meh = defaults.doubleForKey("mehValue")
+        let average = defaults.doubleForKey("averageValue")
+        let awesome = defaults.doubleForKey("awesomeValue")
+        let godlike = defaults.doubleForKey("godlikeValue")
+        
+        
+        var tipPercentages = [meh, average, awesome, godlike]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        
+    }
+    
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+}
--- Tipulator/ViewController.swift
+++ Tipulator/ViewController.swift
@@ -10,20 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var tipControl: UISegmentedControl!
-    
-    @IBOutlet weak var billField: UITextField!
-    
-    @IBOutlet weak var tipLabel: UILabel!
-    
-    @IBOutlet weak var totalLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        tipLabel.text = "$0.00"
-        totalLabel.text = "$0.00"
         
     }
 
@@ -32,35 +21,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
         
     }
-    
-    @IBAction func onEditingChanged(sender: AnyObject) {
-        
-        let defaults = NSUserDefaults.standardUserDefaults()
-        let meh = defaults.doubleForKey("mehValue")
-        let average = defaults.doubleForKey("averageValue")
-        let awesome = defaults.doubleForKey("awesomeValue")
-        let godlike = defaults.doubleForKey("godlikeValue")
-        
-        
-        var tipPercentages = [meh, average, awesome, godlike]
-        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
-        
-        var billAmount = NSString(string: billField.text!).doubleValue
-        var tip = billAmount * tipPercentage
-        var total = billAmount + tip
-    
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-        
-    }
-    
-    
-    @IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
-    
-    }
-    
-
 
 }
 
